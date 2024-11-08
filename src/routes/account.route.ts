4a0a13791677c55b878d73e20831897629ca4d65
Oluwayimika <oladipupomichael9@gmail.com>
2024-11-08 21:17:03 +0100

    chore: types issue

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import auth from '../middlewares/auth';
 import { getAllUsers, getUser } from '../controllers/admin.controller';
-import { getProfile } from '../controllers/account.controller';
+import { getProfile, login, register } from '../controllers/account.controller';
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.get('/users', auth(['admin']), getAllUsers);
 router.get('/users/:id', auth(['admin']), getUser);
 
 router.get('/profile', auth(['user']), getProfile);
+
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
