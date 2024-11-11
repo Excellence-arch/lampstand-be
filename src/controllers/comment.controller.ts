513521c57e434c5d0d247adc473cc226b0e92a56
Oluwayimika <oladipupomichael9@gmail.com>
2024-11-11 21:13:59 +0100

    feat: comment on a post

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Comment from '../models/comment.model';
+import { IRequest } from '../interfaces/response.interface';
 
 const getCommentById = async (req: Request, res: Response) => {
   try {
@@ -15,4 +16,20 @@ const getCommentById = async (req: Request, res: Response) => {
   }
 };
 
-export { getCommentById };
+const makeComment = async (req: IRequest, res: Response) => {
+  try {
+    const { body, post } = req.body;
+    const user = req.user;
+    const newComment = new Comment({ user, body, post });
+    await newComment.save();
+    res.status(201).send({ message: `success` });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send({ message: error.message });
+    } else {
+      res.status(500).send({ message: `Internal Server Error` });
+    }
+  }
+};
+
+export { getCommentById, makeComment };
